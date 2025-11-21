c01ad37f410deccc484f92e07086018f4a9d9447
MohammedKaif278 <dalvikaif@gmail.com>
2025-11-21 16:00:53 +0530

    Food Delivery App Updated

--- Java/OOPs/Encapsulation/Practice_Example/Food_Delivery_App.java
+++ Java/OOPs/Encapsulation/Practice_Example/Food_Delivery_App.java
@@ -1,4 +1,46 @@
 package OOPs.Encapsulation.Practice_Example;
 
-public class Food_Delivery_App {
+class Order
+{
+    private int orderId;
+    private String status;
+
+    public int getOrderId()
+    {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId)
+    {
+        this.orderId=orderId;
+    }
+
+    public String getStatus()
+    {
+        return status;
+    }
+
+    public void setStatus(String status)
+    {
+        if(status.equals("Placed")||status.equals("Dispached")||status.equals("Delivered"))
+        {
+        this.status=status;
+        }
+        else {
+            System.out.println("Invalid Status");
+        }
+    }
+
+}
+public class Food_Delivery_App
+{
+    public static void main(String[] args) {
+        Order o1=new Order();
+        o1.setOrderId(101);
+        o1.setStatus("Placed");
+
+
+        System.out.println("Order ID : "+o1.getOrderId());
+        System.out.println("Status : "+o1.getStatus());
+    }
 }
