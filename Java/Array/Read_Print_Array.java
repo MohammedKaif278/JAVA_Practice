37d2e2c3fc2a48054c588b4b6ba36b71da797dc6
MohammedKaif278 <dalvikaif@gmail.com>
2025-11-27 23:45:18 +0530

    Array Basic Coding Question

---
+++ Java/Array/Read_Print_Array.java
@@ -0,0 +1,25 @@
+package Array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Read_Print_Array
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The Array Size");
+        int size=sc.nextInt();
+        int[] a=new int[size];
+        System.out.println("Enter the "+size+" array elements");
+        for(int i=0;i<=size-1;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+
+        for(int i=0;i<=size-1;i++)
+        {
+            System.out.println(a[i]);
+        }
+        System.out.println(Arrays.toString(a));
+    }
+}
