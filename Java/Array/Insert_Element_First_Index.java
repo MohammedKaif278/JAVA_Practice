529251ec6fe58c1b72fdc950f6274420b1199618
MohammedKaif278 <dalvikaif@gmail.com>
2025-11-28 23:26:39 +0530

    Array - Insert Element at First Index Coding Question

--- Java/Array/Insert_Element_First_Index.java
+++ Java/Array/Insert_Element_First_Index.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Insert_Element_First_Index {
-
     public static int[] insetFirst(int[] a,int n)
     {
         int[] b=new int[a.length+1];
@@ -16,9 +15,6 @@ public class Insert_Element_First_Index {
         b[0]=n;
         return b;
     }
-
-
-
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter the Array Size");
@@ -29,7 +25,6 @@ public class Insert_Element_First_Index {
         {
             a[i]=sc.nextInt();
         }
-
         System.out.println("Enter the new Element");
         int newElement=sc.nextInt();
         int[] b=insetFirst(a,newElement);
