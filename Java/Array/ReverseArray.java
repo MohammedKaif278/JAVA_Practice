cf8bf8335a848681fe410afdc6313d41852403c8
MohammedKaif278 <dalvikaif@gmail.com>
2025-12-01 22:26:21 +0530

    Array - Reverse Array coding Question Addded

--- Java/Array/ReverseArray.java
+++ Java/Array/ReverseArray.java
@@ -4,16 +4,24 @@ import java.util.Arrays;
 
 public class ReverseArray
 {
-    public static void reverserray(int[] a)
+    public static int[] reverserray(int[] a)
     {
-        for(int i=a.length-1;i>=0;i--)
+        int start=0; int end=a.length-1;
+        while(start<end)
         {
-            System.out.println(a[i]+" ");
+            int temp=a[start];
+            a[start]=a[end];
+            a[end]=temp;
 
+            start++;
+            end--;
         }
+        return a;
     }
     public static void main(String[] args) {
         int[] a={4,5,6,7,8,9};
         reverserray(a);
+        System.out.println(Arrays.toString(a));
+
     }
 }
