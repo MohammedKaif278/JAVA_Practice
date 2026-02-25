2a77661f97efeb995b3e7797be4992249afcc3d8
MohammedKaif278 <dalvikaif@gmail.com>
2026-02-25 11:33:37 +0530

    Some changes in Array coding questions

--- Java/Array/Max_In_Array.java
+++ Java/Array/Max_In_Array.java
@@ -8,7 +8,7 @@ public class Max_In_Array
     {
         int max=a[0];
 
-        for(int i=0;i<a.length;i++) {
+        for(int i=1;i<a.length;i++) {
             if(a[i]>max) {
                 max=a[i];
             }
