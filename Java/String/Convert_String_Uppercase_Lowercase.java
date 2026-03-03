f4d1ec7ee6ead87ce629c650b5dada9c94ff86c2
MohammedKaif278 <dalvikaif@gmail.com>
2026-03-03 11:11:37 +0530

    Convert_String_Uppercase_Lowercase Added - String coding questions

---
+++ Java/String/Convert_String_Uppercase_Lowercase.java
@@ -0,0 +1,14 @@
+package String;
+
+public class Convert_String_Uppercase_Lowercase
+{
+    public static void main(String[] args) {
+        String str="Welcome To Naresh IT";
+
+        String upper=str.toUpperCase();
+        String lower=str.toLowerCase();
+        System.out.println("Original String : "+str);
+        System.out.println("Uppercase String : "+upper);
+        System.out.println("Lowercase String : "+lower);
+    }
+}
