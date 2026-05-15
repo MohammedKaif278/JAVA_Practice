e260dc6a0cf8a1215597bd71955787d355e34eef
MohammedKaif278 <dalvikaif@gmail.com>
2026-05-15 10:27:34 +0530

    Zoo : Multilevel Inheritance - Inheritance coding questions

---
+++ Java/OOPs/Inheritance/Multi_Level_Inheritance/Zoo.java
@@ -0,0 +1,29 @@
+package Inheritance.Multi_Level_Inheritance;
+
+class Animal{
+    void eat(){
+        System.out.println("Animal is eating");
+    }
+}
+
+class Dog extends Animal{
+    void bark(){
+        System.out.println("Dog is Barking");
+    }
+}
+
+class Puppy extends Dog{
+    void weep(){
+        System.out.println("Puppy is weeping");
+    }
+}
+
+public class Zoo {
+    public static void main(String[] args) {
+        Puppy p=new Puppy();
+
+        p.eat();
+        p.bark();
+        p.weep();
+    }
+}
