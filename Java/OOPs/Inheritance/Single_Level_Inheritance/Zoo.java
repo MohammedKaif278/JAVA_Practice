487bc0f37f7534dc217cec5ed8d5d2ef3de859a7
MohammedKaif278 <dalvikaif@gmail.com>
2026-05-15 10:19:48 +0530

    Zoo : Single Inheritance - Inheritance coding questions

---
+++ Java/OOPs/Inheritance/Single_Level_Inheritance/Zoo.java
@@ -0,0 +1,22 @@
+package Inheritance.Single_Level_Inheritance;
+
+class Animal{
+    void eat(){
+        System.out.println("Animal is eating");
+    }
+}
+
+class Dog extends Animal{
+    void bark(){
+        System.out.println("Dog is barking");
+    }
+}
+
+public class Zoo {
+    public static void main(String[] args) {
+    Dog d1=new Dog();
+    d1.eat(); //Parent
+    d1.bark(); //Child
+
+    }
+}
